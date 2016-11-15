ac63407e0366d2836c60a9a7fd39a98c7d6addc8
mnaumoski <naumoski.marija@gmail.com>
2016-11-15 14:37:49 -0600

    added api-routes

---
+++ routes/api-routes.js
@@ -0,0 +1,42 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var Burger   = require("../models")["Burger"]; // Pulls out the Character Model
+
+// Routes
+// =============================================================
+module.exports = function(app){
+
+  // Search for Specific Character (or all characters) then provides JSON
+  app.get('/burgers', function(req, res){
+
+     Burger.findAll({})
+          .then(function(result){
+            return res.json(result);
+        })
+
+  });
+
+  // If a user sends data to add a new character...
+  app.post('/api/new', function(req, res){
+
+    // Take the request...
+    var burger = req.body;
+
+    // // Create a routeName
+    // var routeName = burger.name.replace(/\s+/g, '').toLowerCase();
+
+    // Then add burger to the database using sequelize
+    Burger.create({
+      // routeName: routeName,
+      name: burger.name,
+      burger_date: burger.date,
+      devoured: burger.devoured
+
+    });
+
+  })
+}
